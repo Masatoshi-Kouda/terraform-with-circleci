124c77004f204ff1e88ef23b4ad287e929351f06
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-04-29 20:48:55 +0900

    fix

--- .circleci/get-changed-projects.sh
+++ .circleci/get-changed-projects.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-TARGET_PROJECTS=(gmail youtube)
+TARGET_PROJECTS=("gmail" "youtube")
 
 for project in ${TARGET_PROJECTS[@]}; do
   diff_file=$(git --no-pager diff --name-only "origin/master..HEAD" "$project/terraform")
