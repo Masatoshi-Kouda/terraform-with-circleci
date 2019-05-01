d2e9e2684f49a3c2bf3e031a8c895b851a8c75ff
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-02 01:47:48 +0900

    Add pubsub

--- gmail/terraform/gcp/pubsub/main.tf
+++ gmail/terraform/gcp/pubsub/main.tf
@@ -1,10 +1,3 @@
-/**
 resource "google_pubsub_topic" "example" {
   name = "${var.project}-example-topic"
 }
-
-resource "google_pubsub_topic" "example1" {
-  name = "${var.project}-example-topic1"
-}
-**/
-
