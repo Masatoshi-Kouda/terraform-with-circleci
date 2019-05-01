987abc88bc07156de0d5ad768fc2d443787c559b
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-02 02:36:45 +0900

    update

--- scripts/install-tfnotify.sh
+++ scripts/install-tfnotify.sh
@@ -6,3 +6,4 @@ set -o pipefail
 wget https://github.com/mercari/tfnotify/releases/download/v0.3.0/tfnotify_v0.3.0_linux_amd64.tar.gz
 tar -xzf tfnotify_v0.3.0_linux_amd64.tar.gz
 mv tfnotify_v0.3.0_linux_amd64/tfnotify /usr/bin/
+rm -f tfnotify_v0.3.0_linux_amd64.tar.gz
