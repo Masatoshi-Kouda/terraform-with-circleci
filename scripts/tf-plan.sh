04c51a3c4f93e84907a651b29effaf09bac3d6ec
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-02 14:11:18 +0900

    update

--- scripts/tf-plan.sh
+++ scripts/tf-plan.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o pipefail
 
 cd gmail/terraform/gcp/pubsub/$ENVIRONMENT_DIRECTORY
-terraform plan ../ | tfnotify --config ../../../../../tfnotify.yaml plan 
+terraform plan ../ | tfnotify --config ../../../../../tfnotify.yaml plan --message $ENVIRONMENT_DIRECTORY
