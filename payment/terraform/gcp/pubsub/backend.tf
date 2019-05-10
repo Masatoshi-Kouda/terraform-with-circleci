a0cce2d95d2c773b32c73644f409b7e18da83463
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-11 01:58:53 +0900

    update

--- payment/terraform/gcp/pubsub/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "masatoshi-kouda-terraform-state"
-    prefix = "youtube/gcp/pubsub"
-  }
-}
--- payment/terraform/gcp/pubsub/main.tf
+++
@@ -1,3 +0,0 @@
-resource "google_pubsub_topic" "example" {
-  name = "${var.project}-example-topic"
-}
