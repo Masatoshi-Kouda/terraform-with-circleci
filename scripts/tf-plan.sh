a2a316a3f408e4b5c038ae3ae5b0a5d6ab921e14
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-12 03:32:42 +0900

    fix

--- scripts/tf-plan.sh
+++ scripts/tf-plan.sh
@@ -2,14 +2,13 @@
 set -o nounset
 set -o errexit
 set -o pipefail
-set -o xtrace
 
 mkdir -p $HOME/$ENVIRONMENT_DIRECTORY
 echo $GCP_CREDENTIALS | base64 -d > $HOME/$ENVIRONMENT_DIRECTORY/gcp_credentials.json
 export GOOGLE_APPLICATION_CREDENTIALS="$HOME/$ENVIRONMENT_DIRECTORY/gcp_credentials.json"
 
 diff_file=$(git --no-pager diff --name-only "origin/master..HEAD" "terraform/$PROJECT_NAME")
-release_branch=$(git symbolic-ref --short HEAD | grep "^release/$PROJECT_NAME")
+release_branch=$(git symbolic-ref --short HEAD | grep -E "^release/$PROJECT_NAME" || true)
 if [ -n "$diff_file" ] || [ -n "$release_branch" ]; then
     cd terraform/$PROJECT_NAME/gcp/$ENVIRONMENT_DIRECTORY
     terraform plan -out=tfplan ../ | tfnotify --config ../../../../tfnotify/github.yaml plan --message "terraform/$PROJECT_NAME/gcp/$ENVIRONMENT_DIRECTORY"
