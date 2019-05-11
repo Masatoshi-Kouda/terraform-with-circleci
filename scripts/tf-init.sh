281461f019d7abf06aa47bef01f5feccfc6f4032
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-11 17:55:57 +0900

    fix

--- scripts/tf-init.sh
+++ scripts/tf-init.sh
@@ -7,10 +7,5 @@ mkdir -p $HOME/$ENVIRONMENT_DIRECTORY
 echo $GCP_CREDENTIALS | base64 -d > $HOME/$ENVIRONMENT_DIRECTORY/gcp_credentials.json
 export GOOGLE_APPLICATION_CREDENTIALS="$HOME/$ENVIRONMENT_DIRECTORY/gcp_credentials.json"
 
-diff_file=$(git --no-pager diff --name-only "origin/master..HEAD" "terraform/$PROJECT_NAME")
-if [ -n "$diff_file" ]; then
-    cd terraform/$PROJECT_NAME/gcp/$ENVIRONMENT_DIRECTORY
-    terraform init -backend-config backend.tfvars ../
-else
-    echo "Skip terraform init"
-fi
+cd terraform/$PROJECT_NAME/gcp/$ENVIRONMENT_DIRECTORY
+terraform init -backend-config backend.tfvars ../
