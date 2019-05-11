8cbef6622b0ae60ebe68c6d70055e2cb5684c6df
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-11 23:58:25 +0900

    update

--- terraform/payment/gcp/pubsub.tf
+++ terraform/payment/gcp/pubsub.tf
@@ -5,7 +5,3 @@ resource "google_pubsub_topic" "example" {
 resource "google_pubsub_topic" "example1" {
   name = "${var.project}-example-topic1"
 }
-
-resource "google_pubsub_topic" "example2" {
-  name = "${var.project}-example-topic2"
-}
