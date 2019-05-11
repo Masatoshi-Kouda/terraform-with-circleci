f679d22a8f007b49df30419a028543fa6fa00a55
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-11 17:21:59 +0900

    fix

--- scripts/install-tfnotify.sh
+++ scripts/install-tfnotify.sh
@@ -5,7 +5,12 @@ set -o pipefail
 
 tfnotify_version="v0.3.0"
 
-wget https://github.com/mercari/tfnotify/releases/download/$tfnotify_version/tfnotify_${tfnotify_version}_linux_amd64.tar.gz
-tar -xzf tfnotify_${tfnotify_version}_linux_amd64.tar.gz
-mv tfnotify_${tfnotify_version}_linux_amd64/tfnotify /usr/bin/
-rm -f tfnotify_${tfnotify_version}_linux_amd64.tar.gz
+diff_file=$(git --no-pager diff --name-only "origin/master..HEAD" "terraform/$PROJECT_NAME")
+if [ -n "$diff_file" ]; then
+    wget https://github.com/mercari/tfnotify/releases/download/$tfnotify_version/tfnotify_${tfnotify_version}_linux_amd64.tar.gz
+    tar -xzf tfnotify_${tfnotify_version}_linux_amd64.tar.gz
+    mv tfnotify_${tfnotify_version}_linux_amd64/tfnotify /usr/bin/
+    rm -f tfnotify_${tfnotify_version}_linux_amd64.tar.gz
+else
+    echo "Skip install tfnotify"
+fi
