951fcd2d33321147d2834f8348f0b27365418452
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2019-05-12 03:16:58 +0900

    fix

--- scripts/tf-validate.sh
+++ scripts/tf-validate.sh
@@ -10,7 +10,6 @@ export GOOGLE_APPLICATION_CREDENTIALS="$HOME/$ENVIRONMENT_DIRECTORY/gcp_credenti
 
 git --version
 diff_file=$(git --no-pager diff --name-only "origin/master..HEAD" "terraform/$PROJECT_NAME")
-echo "hello"
 release_branch=$(git symbolic-ref --short HEAD | grep -E "^fix")
 if [ -n "$diff_file" ] || [ -n "$release_branch" ]; then
     cd terraform/$PROJECT_NAME/gcp/$ENVIRONMENT_DIRECTORY
